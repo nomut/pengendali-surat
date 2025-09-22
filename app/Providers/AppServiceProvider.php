ddc50261b5238ca5d839fe71e4eede836acbde0f
noval <bruno@asiapowergames.com>
2025-09-23 01:12:29 +0700

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,8 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (Request::header('X-Forwarded-Proto') === 'https') {
-            URL::forceScheme('https');
+        if ($this->app->environment('production')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
         }
     }
 }
